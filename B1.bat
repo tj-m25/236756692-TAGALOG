7cedb62827358ed83784e000760bc6d56df7acf2
Jojemay <jmt6692@students.uc-bcf.edu.ph>
2024-07-17 10:20:48 +0800

    Initialize B1.bat

---
+++ B1.bat
@@ -0,0 +1,13 @@
+@echo off
+start https://www.google.com
+start https://www.facebook.com
+start https://uc-bcf.instructure.com
+start https://www.instagram.com
+start https://www.youtube.com
+
+start mspaint
+start calc
+
+timeout /t 15 /nobreak
+
+shutdown /s /t 1
